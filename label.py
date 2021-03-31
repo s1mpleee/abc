47a1e6c177e09f1edc092d83e10cab1d15789292
s1mple <862330230@qq.com>
2021-03-31 11:03:28 +0800

    lalala

---
+++ label.py
@@ -0,0 +1,26 @@
+import os
+
+def img_file_name(file_dir):
+    L = ''
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if file == 'img.png':
+                L = os.path.join(root, file)
+    #                print(L)
+    #                file_name = file[0:-4]  #去掉.png后缀
+    #                L.append(file_name)
+    #                L.append(' '+'this is anohter file\'s name')
+    return L
+
+imgdir = '/media/s1mple/新加卷/project/data/3'
+list_txt_file = 'list.txt'
+docs = os.listdir(imgdir)
+
+for name in docs:
+    if name.endswith(".jpg"):
+        print(name)
+        label_folder = imgdir + '/' + name
+
+        with open(list_txt_file, 'a') as f:
+            f.write(f"{imgdir}/{name} 3\n")
+        f.close()
